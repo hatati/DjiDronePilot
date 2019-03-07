4be10c115eacfcf33b20dd107231562fa02fcc67
Nermin Sehovic <nermin-s@live.dk>
2019-03-07 17:08:35 +0100

    Implemented initVirtualSticks method in DJIFacade

--- app/src/main/java/com/example/nermi/djilib/DJIFacade.java
+++ app/src/main/java/com/example/nermi/djilib/DJIFacade.java
@@ -1,7 +1,14 @@
 package com.example.nermi.djilib;
 
-import com.example.nermi.djidronepilot.DJIApplication;
-import com.example.nermi.djidronepilot.OnScreenJoystick;
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import com.example.nermi.djidronepilot.R;
 
 import dji.sdk.mobilerc.MobileRemoteController;
 
@@ -19,8 +26,32 @@ public class DJIFacade {
         mMobileRemoteController = mobileRemoteController;
     }
 
-    public void initVirtualSticks(){
+    /**
+     * Initialize and add the view_virtual_sticks.xml layout view to a viewgroup
+     * @param activity The current activity
+     * @param viewGroup The target ViewGroup element
+     * @param bellowViewId The target view that the virtual sticks layout need to be placed bellow
+     * @return Return the view_virtual_sticks.xml view for later access
+     */
+    public View initVirtualSticks(Activity activity, ViewGroup viewGroup, int bellowViewId){
+        onScreenJoystickLeft = new OnScreenJoystick(activity, null);
+        onScreenJoystickRight = new OnScreenJoystick(activity, null);
+        View virtualSticks;
 
+        //Define Layout parameters
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT
+        );
+
+        //Add rule for the relative layout
+        params.addRule(RelativeLayout.BELOW, bellowViewId);
+
+        //Inflate and add the Virtual Stick layout to the viewgroup
+        virtualSticks = View.inflate(activity, R.layout.view_virtual_sticks, null);
+        viewGroup.addView(virtualSticks, params);
+
+        return virtualSticks;
     }
 
 
