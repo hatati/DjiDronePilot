ad321c92673af297f6e06319fc17659b3d9788d5
Nermin Sehovic <nermin-s@live.dk>
2019-05-03 14:39:27 +0200

    Refactored DAIFacade to an Interface

--- app/src/main/java/com/example/nermi/djidronepilot/CameraActivity.java
+++ app/src/main/java/com/example/nermi/djidronepilot/CameraActivity.java
@@ -6,14 +6,14 @@ import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.example.nermi.dailib.DAIFacade;
+import com.example.nermi.dailib.DJIFacade;
 
 public class CameraActivity extends AppCompatActivity {
 
     private final String modelPath = "landing_stripes-CNN-RGB.tflite";
     private final String labelsPath = "labels_landing_stripe.txt";
     BroadcastReceiver smsBroadcastReceiver;
-    DAIFacade daiFacade;
+    DJIFacade djiFacade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +22,9 @@ public class CameraActivity extends AppCompatActivity {
         smsBroadcastReceiver = SmsBroadcastReceiver.getSmsBroadcastReceiver();
 
         if (null == savedInstanceState) {
-            daiFacade = new DAIFacade();
+            djiFacade = DJIFacade.getDjiFacade();
 
-            daiFacade.initDjiUI(this, R.id.container);
+            djiFacade.initUI(this, R.id.container);
 
         }
     }
@@ -37,12 +37,11 @@ public class CameraActivity extends AppCompatActivity {
         IntentFilter intentFilter = new IntentFilter(SmsBroadcastReceiver.SMS_RECEIVED);
         registerReceiver(smsBroadcastReceiver, intentFilter);
 
-        daiFacade.initCNNModel(this, modelPath, labelsPath, 70,70, R.id.container);
-        daiFacade.djiPitchForward(this, R.id.container, "forward", 0.0);
-        daiFacade.djiRollLeft(this, R.id.container, "roll_left", 0.0);
-        daiFacade.djiRollRight(this, R.id.container, "roll_right", 0.0);
-        daiFacade.djiYawLeft(this, R.id.container, "yaw_left", 0.0);
-        daiFacade.djiYawRight(this, R.id.container, "yaw_right", 0.0);
+        djiFacade.pitchForward(this, R.id.container, "forward");
+        djiFacade.rollLeft(this, R.id.container, "roll_left");
+        djiFacade.rollRight(this, R.id.container, "roll_right");
+        djiFacade.yawLeft(this, R.id.container, "yaw_left");
+        djiFacade.yawRight(this, R.id.container, "yaw_right");
 
     }
 
