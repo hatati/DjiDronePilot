0bb8982c5338d870c4b7d18b2cda02d12642aa0c
Nermin Sehovic <nermin-s@live.dk>
2019-05-09 01:59:34 +0200

    Added dailib

---
+++ dailib/src/main/java/com/nermi/dailib/CameraActivity.java
@@ -0,0 +1,53 @@
+package com.nermi.dailib;
+
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private final String modelPath = "landing_stripes-CNN-RGB.tflite";
+    private final String labelsPath = "labels_landing_stripe.txt";
+    BroadcastReceiver smsBroadcastReceiver;
+    DAIFacade daiFacade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        smsBroadcastReceiver = SmsBroadcastReceiver.getSmsBroadcastReceiver();
+
+        if (null == savedInstanceState) {
+            daiFacade = new DAIFacade();
+
+            daiFacade.initDjiUI(this, R.id.container);
+
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Register SMSReceiver
+        IntentFilter intentFilter = new IntentFilter(SmsBroadcastReceiver.SMS_RECEIVED);
+        registerReceiver(smsBroadcastReceiver, intentFilter);
+
+        daiFacade.initCNNModel(this, modelPath, labelsPath, 70,70, R.id.container);
+        daiFacade.djiPitchForward(this, R.id.container, "forward", 0.0);
+        daiFacade.djiRollLeft(this, R.id.container, "roll_left", 0.0);
+        daiFacade.djiRollRight(this, R.id.container, "roll_right", 0.0);
+        daiFacade.djiYawLeft(this, R.id.container, "yaw_left", 0.0);
+        daiFacade.djiYawRight(this, R.id.container, "yaw_right", 0.0);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //Unregister Receivers
+        unregisterReceiver(smsBroadcastReceiver);
+    }
+
+}
---
+++ dailib/src/main/java/com/nermi/dailib/ImageClassifierForwardeftRightCNN.java
@@ -0,0 +1,80 @@
+package com.nermi.dailib;
+
+import android.app.Activity;
+
+import java.io.IOException;
+
+public class ImageClassifierForwardeftRightCNN extends ImageClassifier {
+
+    /**
+     * An array to hold inference results, to be feed into Tensorflow Lite as outputs. This isn't part
+     * of the super class, because we need a primitive array here.
+     */
+    private float[][] labelProbArray = null;
+
+    /**
+     * Initializes an {@code ImageClassifierFloatMobileNet}.
+     *
+     * @param activity
+     */
+    ImageClassifierForwardeftRightCNN(Activity activity) throws IOException {
+        super(activity);
+        labelProbArray = new float[1][getNumLabels()];
+    }
+
+    @Override
+    protected String getModelPath() {
+        // you can download this file from
+        // see build.gradle for where to obtain this file. It should be auto
+        // downloaded into assets.
+        return "forward-left-right-CNN-RGB.tflite";
+    }
+
+    @Override
+    protected String getLabelPath() {
+        return "labels_forward_left_right.txt";
+    }
+
+    @Override
+    protected int getImageSizeX() {
+        return 70;
+    }
+
+    @Override
+    protected int getImageSizeY() {
+        return 70;
+    }
+
+    @Override
+    protected int getNumBytesPerChannel() {
+        return 4; // Float.SIZE / Byte.SIZE;
+    }
+
+    @Override
+    protected void addPixelValue(int pixelValue) {
+        imgData.putFloat(((pixelValue >> 16) & 0xFF) / 255.f);
+        imgData.putFloat(((pixelValue >> 8) & 0xFF) / 255.f);
+        imgData.putFloat((pixelValue & 0xFF) / 255.f);
+    }
+
+    @Override
+    protected float getProbability(int labelIndex) {
+        return labelProbArray[0][labelIndex];
+    }
+
+    @Override
+    protected void setProbability(int labelIndex, Number value) {
+        labelProbArray[0][labelIndex] = value.floatValue();
+    }
+
+    @Override
+
+    protected float getNormalizedProbability(int labelIndex) {
+        return labelProbArray[0][labelIndex];
+    }
+
+    @Override
+    protected void runInference() {
+        tflite.run(imgData, labelProbArray);
+    }
+}
