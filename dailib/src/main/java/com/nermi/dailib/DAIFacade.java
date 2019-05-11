bfb0a043a834960b00284be475c106330767fc47
Nermin Sehovic <nermin-s@live.dk>
2019-05-11 19:42:41 +0200

    Changed DAIFacade to interface in dailib

--- dailib/src/main/java/com/nermi/dailib/DAIFacade.java
+++ dailib/src/main/java/com/nermi/dailib/DAIFacade.java
@@ -1,102 +1,26 @@
-package com.nermi.dailib;
+package com.example.nermi.dailib;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
-import com.nermi.dailib.Camera2Fragment;
-import com.nermi.dailib.DJIApplication;
+public interface DAIFacade {
 
-public class DAIFacade {
+    void initUI(AppCompatActivity activity, int frameLayoutId);
 
-    private Boolean isConnectedToDrone;
-    DJIFacade djiFacade;
+    void pitchForward(AppCompatActivity activity, int frameLayoutId, String label);
 
-    public DAIFacade(){
-        this.isConnectedToDrone = false;
-        djiFacade = DJIFacade.getDjiFacade();
-    }
+    void pitchBackwards(AppCompatActivity activity, int frameLayoutId, String label);
 
-    // DJI methods
-    public void initDjiUI(AppCompatActivity activity, int frameLayoutId){
-        activity.getSupportFragmentManager().beginTransaction().replace(frameLayoutId, Camera2Fragment.newInstance())
-                .commit();
+    void rollLeft(AppCompatActivity activity, int frameLayoutId, String label);
 
-        // Initialize the DJI Mobile remote controller and virtual sticks listeners in the fragment onResume method
-        activity.getSupportFragmentManager().registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
-            @Override
-            public void onFragmentResumed(FragmentManager fm, Fragment f) {
-                super.onFragmentResumed(fm, f);
+    void rollRight(AppCompatActivity activity, int frameLayoutId, String label);
 
-                try{
-                    // Setup the Mobile Remote Controller
-                    djiFacade.setupDJIMobileRemoteController(DJIApplication.getAircraftInstance().getMobileRemoteController());
-                    // Setup virtual sticks listeners
-                    djiFacade.setupVirtualSticksListeners(activity, frameLayoutId);
-                }catch (NullPointerException ex){
-                    ex.printStackTrace();
-                    activity.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast toast = Toast.makeText(activity, "No Mobile Remote Controller connection", Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                    });
-                }
-            }
+    void yawLeft(AppCompatActivity activity, int frameLayoutId, String label);
 
-            @Override
-            public void onFragmentPaused(FragmentManager fm, Fragment f) {
-                super.onFragmentPaused(fm, f);
+    void yawRight(AppCompatActivity activity, int frameLayoutId, String label);
 
-                try{
-                    //Tear down virtual sticks listeners
-                    djiFacade.tearDownVirtualSticksListeners();
-                }catch (NullPointerException ex){
-                    ex.printStackTrace();
-                }
+    void setupMobileRemoteController();
 
-            }
-        }, false);
+    void setupVirtualSticksListeners(AppCompatActivity activity, int frameLayoutId);
 
-    }
-
-    public void initCNNModel(AppCompatActivity activity, String modelPath, String labelsPath, int imageSizeX, int imageSizeY, int frameLayoutId){
-        Camera2Fragment fragment = (Camera2Fragment) activity.getSupportFragmentManager().findFragmentById(frameLayoutId);
-    }
-
-    public void djiPitchForward(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.PITCH_FORWARD);
-
-    }
-
-    public void djiPitchBackwards(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.PITCH_BACKWARDS);
-    }
-
-    public void djiRollRight(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.ROLL_RIGHT);
-    }
-
-    public void djiRollLeft(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.ROLL_LEFT);
-    }
-
-    public void djiYawRight(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.YAW_RIGHT);
-    }
-
-    public void djiYawLeft(AppCompatActivity activity, int frameLayoutId, String label, Double sensitivityRate){
-        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
-                .getLabels().put(label, DroneCommands.YAW_LEFT);
-    }
-
-    //Parrot methods
-    public void initParrotUI(){}
+    void tearDownVirtualSticksListeners();
 }
--- dailib/src/main/java/com/nermi/dailib/DJIFacade.java
+++ dailib/src/main/java/com/nermi/dailib/DJIFacade.java
@@ -1,10 +1,13 @@
 package com.nermi.dailib;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import dji.sdk.mobilerc.MobileRemoteController;
 
-public class DJIFacade {
+public class DJIFacade implements com.example.nermi.dailib.DAIFacade {
 
     private OnScreenJoystick onScreenJoystickRight;
     private OnScreenJoystick onScreenJoystickLeft;
@@ -25,8 +28,92 @@ public class DJIFacade {
 
     }
 
-    public void setupDJIMobileRemoteController(MobileRemoteController mobileRemoteController){
+    private void setupDJIMobileRemoteController(MobileRemoteController mobileRemoteController){
             mMobileRemoteController = mobileRemoteController;
+    }
+
+    @Override
+    public void initUI(AppCompatActivity activity, int frameLayoutId) {
+        activity.getSupportFragmentManager().beginTransaction().replace(frameLayoutId, Camera2Fragment.newInstance())
+                .commit();
+
+        // Initialize the DJI Mobile remote controller and virtual sticks listeners in the fragment onResume method
+        activity.getSupportFragmentManager().registerFragmentLifecycleCallbacks(new FragmentManager.FragmentLifecycleCallbacks() {
+            @Override
+            public void onFragmentResumed(FragmentManager fm, Fragment f) {
+                super.onFragmentResumed(fm, f);
+
+                try{
+                    // Setup the Mobile Remote Controller
+                    setupDJIMobileRemoteController(DJIApplication.getAircraftInstance().getMobileRemoteController());
+                    // Setup virtual sticks listeners
+                    setupVirtualSticksListeners(activity, frameLayoutId);
+                }catch (NullPointerException ex){
+                    ex.printStackTrace();
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast toast = Toast.makeText(activity, "No Mobile Remote Controller connection", Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFragmentPaused(FragmentManager fm, Fragment f) {
+                super.onFragmentPaused(fm, f);
+
+                try{
+                    //Tear down virtual sticks listeners
+                    tearDownVirtualSticksListeners();
+                }catch (NullPointerException ex){
+                    ex.printStackTrace();
+                }
+
+            }
+        }, false);
+    }
+
+    @Override
+    public void pitchForward(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.PITCH_FORWARD);
+    }
+
+    @Override
+    public void pitchBackwards(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.PITCH_BACKWARDS);
+    }
+
+    @Override
+    public void rollLeft(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.ROLL_LEFT);
+    }
+
+    @Override
+    public void rollRight(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.ROLL_RIGHT);
+    }
+
+    @Override
+    public void yawLeft(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.YAW_LEFT);
+    }
+
+    @Override
+    public void yawRight(AppCompatActivity activity, int frameLayoutId, String label) {
+        ((Camera2Fragment)activity.getSupportFragmentManager().findFragmentById(frameLayoutId))
+                .getLabels().put(label, DroneCommands.YAW_RIGHT);
+    }
+
+    @Override
+    public void setupMobileRemoteController() {
+
     }
 
     /**
